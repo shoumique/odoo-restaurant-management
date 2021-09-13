559dcdbfe8e89a602b7faf9c19726cff667fd4ba
sartazshovon <60311592+sartazshovon@users.noreply.github.com>
2021-09-13 18:35:36 +0600

    Add files via upload

---
+++ restaurant_module/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Splot Restaurant",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+    'sequence':5 ,
+    'author': "Sartaz & Shoumique",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','hr'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/employee.xml',
+        'views/addons.xml',
+        'views/kitchen.xml'
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application' : True,
+}
